b75c069b99dabdd1da3396d0530af7d269f21dbb
novasky0709 <novasky0709>
2021-05-25 19:52:14 -0700

    GaussNewton

---
+++ build/CMakeFiles/GaussNewton.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaussNewton.dir/GaussNewton.cpp.o"
+  "GaussNewton"
+  "GaussNewton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaussNewton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
